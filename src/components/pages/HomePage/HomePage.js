10a614d7f2db5d0bcd79d84f103ed492d86dc25a
Senad Djerlek <senajerlek@gmail.com>
2022-10-02 15:10:36 +0200

    added pagination and tailwind

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import { NavLink, Link } from "react-router-dom";
 const HomePage = () => {
-  return <p>Pocetna</p>;
+  return (
+    <>
+      <div>
+        <h1>LOGO</h1>
+        <h3>COVID STATS</h3>
+        <h3>ABOUT US</h3>
+        <form>
+          <input type="search" name="search your state"></input>
+        </form>
+      </div>
+      <NavLink className="no-underline" to={"/stats"}>
+        Click me to go to stats
+      </NavLink>
+    </>
+  );
 };
 
 export default HomePage;
