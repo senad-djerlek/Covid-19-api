43aa61930d266ff2ca3cd9526c97ff1360b0f2d7
Senad Djerlek <senajerlek@gmail.com>
2022-10-07 20:13:43 +0200

    added little changes

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+
+
 const HomePage = () => {
   return (
     <div className="h-screen w-full">
@@ -7,8 +9,6 @@ const HomePage = () => {
         className="w-full"
         src="https://www.bloomberg.com/graphics/2020-coronavirus-cases-world-map/img/2020-coronavirus-cases-world-map-facebook.png?t=202011091543"
         // style={{ objectFit: "contain" }}
-
-        
       ></img>
     </div>
   );
