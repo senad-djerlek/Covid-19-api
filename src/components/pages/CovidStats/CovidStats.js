0b6a8002ee0b260ac1922c1a0eb530e9ee5843f2
Senad Djerlek <senajerlek@gmail.com>
2022-10-07 18:52:47 +0200

    added Pagination

--- src/components/pages/CovidStats/CovidStats.js
+++ src/components/pages/CovidStats/CovidStats.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { NavLink } from "react-router-dom";
+import Typography from "@mui/material/Typography";
+
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+
 const CovidStats = () => {
   const [getCovidStats, setGetCovidStats] = useState([]);
   const [activePage, setPage] = useState(1);
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
   const options = {
     method: "GET",
     url: "https://covid-193.p.rapidapi.com/statistics",
@@ -49,18 +57,23 @@ const CovidStats = () => {
       <NavLink to={"/"}> Click me to go back to homepage</NavLink>
       {getCovidStats
         .map((el) => (
-          <p key={el.id}>
-            <p>
+          <ul key={el.id}>
+            <li>
               Continent Name: {el.continent} Country Name: {el.country}{" "}
               Population: {el.population} New Cases:{el.cases.new} Deaths:
               {el.deaths.total}
-            </p>
-          </p>
+            </li>
+          </ul>
         ))
         .slice(visitedPerPage, visitedPerPage + perPage)}
       {/* <Pagination page={activePage} onChange={setPage} total={totalPages} />{" "} */}
-      <p>Covid Stats</p>
-      <h2>smao jhionaslov</h2>
+      <Stack spacing={2}>
+        <Pagination
+          page={activePage}
+          count={totalPages}
+          onChange={handleChange}
+        ></Pagination>
+      </Stack>
     </div>
   );
 };
